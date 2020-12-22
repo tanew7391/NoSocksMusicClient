d0bcc505396fb484650875f08a33c8f27bb3d399
Taylor Newman <newmant@uoguelph.ca>
2020-12-21 22:32:51 -0500

    Migrated server over

---
+++ server/index.js
@@ -0,0 +1,89 @@
+// server.js
+const express = require('express');
+const path = require("path");
+const app = express();
+const SpotifyWebApi = require('spotify-web-api-node');
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.SERVER_PORT || "5000";
+const scopeList = ["user-follow-modify", "user-follow-read"]
+
+
+// init Spotify API wrapper
+var redirectUri = `http://localhost:${port}/callback`;
+var tokenExpirationEpoch;
+var spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: redirectUri
+});
+var tokenExpirationEpoch;
+
+app.set("views", path.join(__dirname, "viewsExpress"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "publicExpress")));
+
+
+
+app.get("/", (req, res) => {
+    if(!tokenExpirationEpoch){
+        res.render("authorize", { title: "Authorize" });
+    } else if(tokenExpirationEpoch > (new Date().getTime() / 1000)){
+        spotifyApi.refreshAccessToken().then(spotifyApi.refreshAccessToken().then(
+            function(data) {
+              console.log('The access token has been refreshed!');
+              // Save the access token so that it's used in future calls
+              spotifyApi.setAccessToken(data.body['access_token']);
+            },
+            function(err) {
+              console.log('Could not refresh access token', err);
+            }
+          ));
+    } else {
+        res.render("main-page");
+    }
+});
+
+app.get("/authorize", (req, res) => {
+    var authorizeURL = spotifyApi.createAuthorizeURL(scopeList);
+    console.log(scopeList)
+    console.log(authorizeURL)
+    res.redirect(authorizeURL);
+});
+
+
+app.get('/api/greeting', (req, res) => {
+    const name = req.query.name || 'World';
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ greeting: `Goodbye ${name}!` }));
+  });
+
+// Exchange Authorization Code for an Access Token
+app.get("/callback", (req, res) => {
+    var authorizationCode = req.query.code;
+    // Check folks haven't just gone direct to the callback URL
+    if (!authorizationCode) {
+        res.redirect('/');
+    } else {
+        res.render("callback", { title: "Callback" });
+    }
+
+    spotifyApi.authorizationCodeGrant(authorizationCode).then((data) => {
+        // Set the access token and refresh token
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.setRefreshToken(data.body['refresh_token']);
+
+        // Save the amount of seconds until the access token expired
+        tokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
+        console.log('Retrieved token. It expires in ' + Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+    }, (err) => {
+        console.log('Something went wrong when retrieving the access token!', err.message);
+    });
+});
+
+
+// listen for requests :)
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.value)
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.greeting}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MyApp;
